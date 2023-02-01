c5d79c70d94a968d89804cad04fac935d8da93c9
DiasUte <201289@astanait.edu.kz>
2023-02-01 14:30:04 +0600

    2.2v

--- src/components/Publication/Publication.jsx
+++ src/components/Publication/Publication.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import {clrs} from "../../constants/colors";
+import cl from "./Publication.module.css"
 
 const Publication = ({id, text}) => {
     return (
-        <div style={{width: 'calc(26.66%)', backgroundColor: id%2 === 0 ? clrs.plsCardColor1 : clrs.plsCardColor2, margin: '20px 40px 20px 40px'}}>
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                flexDirection: 'column',
-                padding: '20px',
-                gap: '20px'
-            }}>
-                <p style={{textAlign: 'center', color: id%2 === 0 ? clrs.black : clrs.white}}>Correspondence between dental and skeletal maturity
-                    parameters among patients with different sagittal relationships at the end of puberty
-                    period, Journal of International Dental and Medical Research, 2020, 13(1), pp.
-                    223–228</p>
-            </div>
+        <div className={cl.card} style={{backgroundColor: id % 2 === 0 ? clrs.silver : clrs.green}}>
+            <p style={{color: id % 2 === 0 ? clrs.black : clrs.white}}>Correspondence between
+                dental and skeletal maturity
+                parameters among patients with different sagittal relationships at the end of puberty
+                period, Journal of International Dental and Medical Research, 2020, 13(1), pp.
+                223–228</p>
         </div>
     );
 };
