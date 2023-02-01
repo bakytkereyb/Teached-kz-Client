f6c65fd290bea2bd97930829e6405458c62f5740
Batyrbek <https://github.com/bakytkereyb/assignment4.git>
2023-02-01 14:42:47 +0600

    2.2v

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <HomePage/>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
