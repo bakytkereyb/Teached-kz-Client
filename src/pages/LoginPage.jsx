3bf693bf5ae6ed01f2f72db1c6e458ac56afa0ea
DiasUte <201289@astanait.edu.kz>
2023-02-05 21:14:57 +0600

    2.3v (Login and Register Pages)

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import Header from "../components/Header/Header";
+import Block from "../components/UI/Block/Block";
+import BigText from "../components/UI/BigText/BigText";
+import FormBlock from "../components/Form/FormBlock";
+import FormInput from "../components/Form/FormInput";
+import Button from "../components/UI/Button/Button";
+import Footer from "../components/Footer/Footer";
+import Text from "../components/UI/Text/Text";
+import {lan} from "../constants/lan";
+import {clrs} from "../constants/colors";
+
+const LoginPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function onSubmitLogin(e) {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <Header/>
+            <Block>
+                <BigText style={{fontWeight: 600}}>{lan.login}</BigText>
+            </Block>
+            <Block>
+                <FormBlock onSubmit={onSubmitLogin}>
+                    <FormInput
+                        labelText={"Username"}
+                        value={username}
+                        onChange={setUsername}
+                        id={"username"}
+                        type={"text"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Password"}
+                        value={password}
+                        onChange={setPassword}
+                        id={"password"}
+                        type={"password"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <Button type={1}>Login</Button>
+                    <Text style = {{color: clrs.blackBlue}}>
+                        {lan.noAccount}
+                    </Text>
+                </FormBlock>
+            </Block>
+            <Footer/>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/pages/RegistrationPage.jsx
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+import Header from "../components/Header/Header";
+import Block from "../components/UI/Block/Block";
+import BigText from "../components/UI/BigText/BigText";
+import FormBlock from "../components/Form/FormBlock";
+import FormInput from "../components/Form/FormInput";
+import Button from "../components/UI/Button/Button";
+import Text from "../components/UI/Text/Text";
+import {lan} from "../constants/lan";
+import Footer from "../components/Footer/Footer";
+import {clrs} from "../constants/colors";
+
+const RegistrationPage = () => {
+
+    const [username, setUsername] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [secondName, setSecondName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+
+    function onSubmitLogin(e) {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <Header/>
+            <Block style={{background: "red"}}>
+                <BigText style={{fontWeight: 600}}>{lan.register}</BigText>
+            </Block>
+
+            <Block>
+                <FormBlock onSubmit={onSubmitLogin}>
+                    <FormInput
+                        labelText={"First Name"}
+                        value={firstName}
+                        onChange={setFirstName}
+                        id={"firstName"}
+                        type={"text"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Second Name"}
+                        value={secondName}
+                        onChange={setSecondName}
+                        id={"secondName"}
+                        type={"text"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Email"}
+                        value={email}
+                        onChange={setEmail}
+                        id={"email"}
+                        type={"text"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Username"}
+                        value={username}
+                        onChange={setUsername}
+                        id={"username"}
+                        type={"text"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Password"}
+                        value={password}
+                        onChange={setPassword}
+                        id={"password"}
+                        type={"password"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <FormInput
+                        labelText={"Repeat Password"}
+                        value={password2}
+                        onChange={setPassword2}
+                        id={"password2"}
+                        type={"password"}
+                        required={true}
+                        maxWidth={"50%"}
+                    />
+                    <Button type={1}>Register</Button>
+                    <Text style = {{color: clrs.blackBlue}}>
+                        {lan.yesAccount}
+                    </Text>
+                </FormBlock>
+            </Block>
+            <Footer/>
+        </div>
+    );
+};
+
+export default RegistrationPage;
