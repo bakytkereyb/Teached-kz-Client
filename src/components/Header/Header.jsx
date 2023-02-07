a147b3e49aee0d1b846099c800c271e47991ded2
DiasUte <201289@astanait.edu.kz>
2023-02-07 20:45:24 +0600

    2.3v (Header v2)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,9 @@ import {clrs} from "../../constants/colors"
 import Button from "../UI/Button/Button";
 import Select from "../UI/Select/Select";
 import Logo from "../Logo/Logo";
-import {Drawer} from "antd";
+import {Divider, Drawer} from "antd";
 import burger from "../../images/hamburger.svg"
+import BigText from "../UI/BigText/BigText";
 
 
 const Header = () => {
@@ -24,13 +25,24 @@ const Header = () => {
         <header className={cl.header}>
             <div className={cl.wrapper}>
                 <div className={cl.menu}>
-                    <img src={burger} alt="" onClick={showDrawer} />
-                    <Logo style={{marginRight: "0 auto"}}/>
+                    <img src={burger} alt="" onClick={showDrawer}/>
                 </div>
                 <Drawer
                     placement="left"
+                    closable={false}
                     onClose={onClose} open={open}>
-                    <p>Dias</p>
+                    <div className={cl.menu__items}>
+                        <Logo/>
+                        <Divider>CONTENT</Divider>
+                        <BigText style={{color: clrs.blackBlue}}>{lan.aboutUs}</BigText>
+                        <BigText style={{color: clrs.blackBlue}}>{lan.team}</BigText>
+                        <BigText style={{color: clrs.blackBlue}}>{lan.publications}</BigText>
+                        <BigText style={{color: clrs.blackBlue}}>{lan.contacts}</BigText>
+                        <BigText style={{color: clrs.blackBlue}}>email@email.com</BigText>
+                        <Divider />
+                        <Select/>
+                        <Button colorText={clrs.white} colorBack={clrs.red}>Log in</Button>
+                    </div>
                 </Drawer>
                 <div className={cl.header__left}>
                     <Text style={{color: clrs.blackBlue}}>{lan.aboutUs}</Text>
