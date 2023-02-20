17eba5ce24b75710fa5918bd64f7c6384433e135
Batyrbek <bakytkerey.b@gmail.com>
2023-02-20 19:23:19 +0600

    page loader

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {clrs} from "../constants/colors";
 import HeaderPlatform from "../components/HeaderPlatform/HeaderPlatform";
 import Block from "../components/UI/Block/Block";
 import BigText from "../components/UI/BigText/BigText";
 import HorizontalDivider from "../components/UI/Divider/HorizontalDivider";
+import PageLoader from "../components/PageLoader/PageLoader";
 
 const DashboardPage = () => {
+
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000)
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Block>
+                <PageLoader/>
+            </Block>
+        )
+    }
 
     return (
         <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
