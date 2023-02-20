838f7d8a11aa5522fc57e9e2097857eb04481d80
Batyrbek <bakytkerey.b@gmail.com>
2023-02-20 19:17:52 +0600

    platform dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {clrs} from "../constants/colors";
 import HeaderPlatform from "../components/HeaderPlatform/HeaderPlatform";
+import Block from "../components/UI/Block/Block";
+import BigText from "../components/UI/BigText/BigText";
+import HorizontalDivider from "../components/UI/Divider/HorizontalDivider";
 
 const DashboardPage = () => {
 
     return (
         <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
             <HeaderPlatform/>
+            <Block style={{marginTop: "50px"}}>
+                {/*  here  */}
+            </Block>
         </div>
     );
 };
