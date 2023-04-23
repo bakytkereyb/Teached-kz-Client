b76efb621645c145faf2d6576ffaf045ba3d5094
Batyrbek <bakytkerey.b@gmail.com>
2023-04-23 13:55:35 +0600

    feat: logout loading

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {setUser} from "../store/slices/userSlice";
+import FullLoading from "../components/LoadingComponents/FullLoading";
 
 const Logout = () => {
     const dispatch = useDispatch();
@@ -14,9 +15,7 @@ const Logout = () => {
     }, [])
 
     return (
-        <div>
-
-        </div>
+        <FullLoading/>
     );
 };
 
