f45a6f8e2412c4b221467b3ea664cda7e7174a46
Batyrbek <bakytkerey.b@gmail.com>
2023-05-08 16:09:15 +0600

    feat: new reducers

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,8 @@ import competence from "./slices/competenceSlice";
 import adminCourse from "./slices/admin/adminCourseSlice";
 import trainersList from "./slices/trainerListSlice";
 import adminUser from "./slices/admin/adminUserSlice"
+import questionnaire from "./slices/anketaSlice"
+import adminCoursesTableController from "./slices/tableController/AdminCoursersTableController"
 
 export default configureStore({
     reducer: {
@@ -16,5 +18,7 @@ export default configureStore({
         adminCourse: adminCourse,
         trainersList: trainersList,
         adminUser: adminUser,
+        questionnaire: questionnaire,
+        adminCoursesTableController: adminCoursesTableController,
     },
 });
