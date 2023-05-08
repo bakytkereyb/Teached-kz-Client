70abaf54ed10e6f4aed9df6b80e509dfc608daa7
Batyrbek <bakytkerey.b@gmail.com>
2023-05-08 17:39:13 +0600

    feat: courses list slice

---
+++ src/store/slices/coursesSlice.js
@@ -0,0 +1,49 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import CourseService from "../../services/CourseService";
+
+let initialState = {
+    courses: [],
+    isLoading: true,
+    error: null,
+    hasMore: false,
+}
+
+export const getAllPublicCourses = createAsyncThunk(
+    'getAllPublicCourses',
+    async ({ page, limit }, { getState, rejectWithValue, dispatch }) => {
+        try {
+            const response = await CourseService.getAllPublicCourses(page, limit);
+            return response;
+        } catch (error) {
+            return rejectWithValue(error?.response?.data);
+        }
+    },
+);
+
+const coursesSlice = createSlice({
+    name: 'coursesSlice',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllPublicCourses.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+                state.courses = [];
+                state.hasMore = false;
+            })
+            .addCase(getAllPublicCourses.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.courses = action.payload.data;
+                state.hasMore = action.payload.hasMore;
+            })
+            .addCase(getAllPublicCourses.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+    }
+});
+
+export const {} = coursesSlice.actions;
+
+export default coursesSlice.reducer;
