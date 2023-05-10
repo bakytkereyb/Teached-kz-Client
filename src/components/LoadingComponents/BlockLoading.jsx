ab3e16dccd7849c3895a0b0d1d27a6bde8681f69
Batyrbek <bakytkerey.b@gmail.com>
2023-05-11 00:46:33 +0600

    feat: block loading changes

--- src/components/LoadingComponents/BlockLoading.jsx
+++ src/components/LoadingComponents/BlockLoading.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './BlockLoading.module.css';
 
-const BlockLoading = ({isLoading}) => {
+const BlockLoading = ({isLoading, ...props}) => {
     if (!isLoading) {
         return '';
     }
     return (
-        <div className={classes.blockLoading}>
+        <div style={props.style} className={classes.blockLoading}>
             <div className={classes.loader}></div>
         </div>
     );
