c00145e5b276cc4225a5f617a1b045a8f2603b7f
Batyrbek <bakytkerey.b@gmail.com>
2023-05-11 00:48:27 +0600

    feat: course page changes

---
+++ src/pages/Course/CoursePage.jsx
@@ -0,0 +1,87 @@
+import React, {useEffect, useState} from 'react';
+import {clrs} from "../../constants/colors";
+import HeaderPlatform from "../../components/HeaderPlatform/HeaderPlatform";
+import Block from "../../components/UI/Block/Block";
+import CourseHeader from "./CourseHeader/CourseHeader";
+import Card from "../../components/LoadingComponents/Card";
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigate, useParams} from 'react-router-dom';
+import {getCourseById} from '../../store/slices/course/courseSlice';
+import BigText from '../../components/UI/BigText/BigText';
+import {lan} from '../../constants/lan';
+import CourseViewInfo from './CourseView/CourseViewInfo';
+import CourseViewButtons from './CourseView/CourseViewButtons';
+import CourseViewLessons from './CourseView/CourseViewLessons';
+import CourseLessons from './CourseLessons/CourseLessons';
+
+const CoursePage = ({task, student, post, test, lesson}) => {
+
+    const [isComponent, setIsComponent] = useState(1);
+
+    const {course, isLoading} = useSelector(state => state.course);
+    const [tabNum, setTabNum] = useState(1);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        dispatch(getCourseById({id: id}));
+    }, [navigate]);
+
+    useEffect(() => {
+        console.log(course)
+    }, [course]);
+
+    if (isLoading) {
+        return (
+            <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
+                <HeaderPlatform/>
+                <Block style={{marginTop: "50px"}}>
+                    <Card type={"horizontal-circle"}/>
+                    <Card type={"horizontal-small"}/>
+                    <Card type={"horizontal"}/>
+                </Block>
+            </div>
+        );
+    }
+
+    if (course === null) {
+        return (
+            <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
+                <HeaderPlatform/>
+                <Block style={{marginTop: "50px"}}>
+                    <BigText>{lan.courseNotFound}</BigText>
+                </Block>
+            </div>
+        );
+    }
+
+    if (course.userStatus === 'FORBIDDEN') {
+        return (
+            <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
+                <HeaderPlatform/>
+                <Block style={{marginTop: "50px"}}>
+                    <BigText>{lan.courseFORBIDDEN}</BigText>
+                </Block>
+            </div>
+        );
+    }
+
+
+    return (
+        <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
+            <HeaderPlatform/>
+            <Block style={{marginTop: "50px"}}>
+                <CourseHeader/>
+                <CourseViewInfo/>
+                <CourseViewButtons tabNum={tabNum} setTabNum={setTabNum}/>
+
+                {tabNum === 1 && <CourseLessons/>}
+            </Block>
+        </div>
+    );
+};
+
+export default CoursePage;
