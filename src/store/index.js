bf603cd4b9afa9161fceee9ebf4bb043dd4d7695
Batyrbek <bakytkerey.b@gmail.com>
2023-05-14 01:50:12 +0600

    feat: new reducers

--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,20 @@ import tabBlock from "./slices/tabBlock/tabBlockSlice";
 import competence from "./slices/competenceSlice";
 import publicCompetence from "./slices/publicCompetenceSlice";
 import adminCourse from "./slices/admin/adminCourseSlice";
+import adminPostCourse from "./slices/admin/adminPostCourseSlice";
 import courses from "./slices/coursesSlice";
+import postCourses from "./slices/postCoursesSlice";
 import trainersList from "./slices/trainerListSlice";
 import adminUser from "./slices/admin/adminUserSlice"
 import questionnaire from "./slices/anketaSlice"
 import courseStudents from "./slices/courseStudentsSlice"
+import postCourseStudents from "./slices/postCourseStudentsSlice"
 import adminCoursesTableController from "./slices/tableController/AdminCoursersTableController"
 import adminUsersTableController from "./slices/tableController/AdminUsersTableController"
 import coursesTableController from "./slices/tableController/CoursesTableController"
 import myCoursesTableController from "./slices/tableController/MyCoursesTableController"
 import course from "./slices/course/courseSlice"
+import postCourse from "./slices/postCourse/postCourseSlice"
 import chatUsersTableController from "./slices/tableController/ChatUsersTableController";
 import task from "./slices/taskSlice";
 import tasks from "./slices/tasksSlice";
@@ -31,7 +35,9 @@ export default configureStore({
         competenceBank: competence,
         publicCompetenceBank: publicCompetence,
         adminCourse: adminCourse,
+        adminPostCourse: adminPostCourse,
         courses: courses,
+        post_courses: postCourses,
         trainersList: trainersList,
         adminUser: adminUser,
         questionnaire: questionnaire,
@@ -40,12 +46,14 @@ export default configureStore({
         coursesTableController: coursesTableController,
         myCoursesTableController: myCoursesTableController,
         course: course,
+        postCourse: postCourse,
         chatUsersTableController: chatUsersTableController,
         courseStudentsViewController: courseStudentsViewController,
         task: task,
         tasks: tasks,
         taskFiles: taskFiles,
         courseStudents: courseStudents,
+        postCourseStudents: postCourseStudents,
         trainingCourses: trainingCourses,
         trainingCoursesTableController: trainingCoursesTableController,
     },
