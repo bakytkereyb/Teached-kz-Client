43049b7d0f6c8c65fbcecafe0faeb447395dbe4d
Batyrbek <bakytkerey.b@gmail.com>
2023-05-14 17:11:05 +0600

    feat: chat list page

--- src/pages/Chat/ChatList.jsx
+++ src/pages/Chat/ChatList.jsx
@@ -1,30 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TableWithPagination from "../../components/TableWithPagination/TableWithPagination";
 import {useDispatch, useSelector} from "react-redux";
 import {Link, useNavigate} from "react-router-dom";
-import {getAllUsers} from "../../store/slices/admin/adminUserSlice";
-import {changeCurrentPage} from "../../store/slices/tableController/ChatUsersTableController";
 import {lan} from "../../constants/lan";
 import Text from "../../components/UI/Text/Text";
-import {Badge} from "antd";
 import FlexBlock from "../../components/UI/FlexBlock/FlexBlock";
 import {clrs} from "../../constants/colors";
 import HeaderPlatform from "../../components/HeaderPlatform/HeaderPlatform";
 import Block from "../../components/UI/Block/Block";
-import FormInput from "../../components/Form/FormInput";
-import FormBlock from "../../components/Form/FormBlock";
-import Button from "../../components/UI/Button/Button";
+import {changeCurrentPage} from '../../store/slices/tableController/MyChatsTableController';
+import {getAllMyChats} from '../../store/slices/chat/chatsSlice';
 
 const ChatList = () => {
 
-    const {users, hasMore, isLoading} = useSelector(state => state.adminUser)
-    const {currentPage, pageSize} = useSelector(state => state.chatUsersTableController);
+    const {chats, hasMore, isLoading} = useSelector(state => state.chats)
+    const {currentPage, pageSize} = useSelector(state => state.myChatsTableController);
     const [searchName, setSearchName] = useState('')
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        dispatch(changeCurrentPage({ page: 1, limit: 5 }));
+        dispatch(getAllMyChats({ page: 1, limit: 5 }));
+    }, [navigate])
+
     const fetchData = async (params) => {
-        return dispatch(getAllUsers(params));
+        return dispatch(getAllMyChats(params));
     };
 
     const saveCurrentPageSettings = (page, pageSize) => {
@@ -34,22 +35,12 @@ const ChatList = () => {
     const columns = [
         {
             title: lan.fullName + ` (${lan.username})`,
-            render: (_, record) => <Text normalWeight>{record.fullName} <b>({record.username})</b></Text>,
-        },
-        {
-            title: lan.fullFilled,
-            render: (_, record) => (
-                record.fullFilled ?
-                    <Badge status="success" text={lan.filled}/>
-                    :
-                    <Badge status="default" text={lan.notFilled}/>
-            ),
-            width: '10%',
+            render: (_, record) => <Text normalWeight>{record.user.fullName} <b>({record.user.username})</b></Text>,
         },
         {
             title: lan.profile,
             render: (_, record) => (
-                <Link to={`/profile/${record.username}`} target="_blank">{lan.view}</Link>
+                <Link to={`/profile/${record.user.username}`} target="_blank">{lan.view}</Link>
             ),
             width: '5%',
         },
@@ -57,56 +48,62 @@ const ChatList = () => {
             title: lan.actions,
             render: (_, record) => (
                 <FlexBlock>
-                    <Text type={"button"}>{lan.view}</Text>
-                    {
-                        record.admin ?
-                            <Text type={"button-green"}>{lan.trainer}</Text>
-                            :
-                            <Text type={"button-red"}>{lan.admin}</Text>
-                    }
+                    <Text onClick={() => {navigate(`/chat/${record.id}`)}} type={"button"}>{lan.view}</Text>
                 </FlexBlock>
             ),
-            width: '20%',
+            width: '10%',
         },
     ];
 
     async function onSubmitSearchUsers(e) {
         e.preventDefault();
-        console.log(searchName)
-        alert(searchName)
-        dispatch(changeCurrentPage({page: 1, pageSize: 1}));
-
-        dispatch(getAllUsers({page: 1, limit: 1}));
     }
 
     return (
         <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
             <HeaderPlatform/>
-            <Block style={{marginTop: "50px"}}>
+            <Block style={{marginTop: "50px", alignItems: "flex-start"}}>
                 <Text style={{textTransform: "uppercase", fontSize: "1rem"}}>{lan.chats}</Text>
-                <FormBlock style={{flexDirection: 'row', justifyContent: 'start'}} onSubmit={onSubmitSearchUsers}>
-                    <FormInput
-                        labelText={lan.fullName}
-                        id={"searchName"}
-                        type={"text"}
-                        required={false}
-                        value={searchName}
-                        onChange={setSearchName}
-                        maxWidth={"20%"}
-                        withoutLabel={true}
+                <FlexBlock style={{
+                    backgroundColor: clrs.white,
+                    borderRadius: "15px",
+                    padding: "20px",
+                    width: "calc(100% - 40px)",
+                }}>
+                    <TableWithPagination
+                        isLoading={isLoading}
+                        dataSource={chats}
+                        columns={columns}
+                        fetchData={fetchData}
+                        saveCurrentPage={saveCurrentPageSettings}
+                        initialPage={currentPage}
+                        initialPageSize={pageSize}
+                        hasMore={hasMore}
                     />
-                    <Button>Search</Button>
-                </FormBlock>
-                <TableWithPagination
-                    isLoading={isLoading}
-                    dataSource={users}
-                    columns={columns}
-                    fetchData={fetchData}
-                    saveCurrentPage={saveCurrentPageSettings}
-                    initialPage={currentPage}
-                    initialPageSize={pageSize}
-                    hasMore={hasMore}
-                />
+                </FlexBlock>
+                {/*<FormBlock style={{flexDirection: 'row', justifyContent: 'start'}} onSubmit={onSubmitSearchUsers}>*/}
+                {/*    <FormInput*/}
+                {/*        labelText={lan.fullName}*/}
+                {/*        id={"searchName"}*/}
+                {/*        type={"text"}*/}
+                {/*        required={false}*/}
+                {/*        value={searchName}*/}
+                {/*        onChange={setSearchName}*/}
+                {/*        maxWidth={"20%"}*/}
+                {/*        withoutLabel={true}*/}
+                {/*    />*/}
+                {/*    <Button>Search</Button>*/}
+                {/*</FormBlock>*/}
+                {/*<TableWithPagination*/}
+                {/*    isLoading={isLoading}*/}
+                {/*    dataSource={users}*/}
+                {/*    columns={columns}*/}
+                {/*    fetchData={fetchData}*/}
+                {/*    saveCurrentPage={saveCurrentPageSettings}*/}
+                {/*    initialPage={currentPage}*/}
+                {/*    initialPageSize={pageSize}*/}
+                {/*    hasMore={hasMore}*/}
+                {/*/>*/}
             </Block>
         </div>
     );
