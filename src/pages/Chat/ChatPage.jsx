ab0d472f946594a8faf99c9c0b81baa19b224045
Batyrbek <bakytkerey.b@gmail.com>
2023-05-14 17:11:11 +0600

    feat: chat page

--- src/pages/Chat/ChatPage.jsx
+++ src/pages/Chat/ChatPage.jsx
@@ -1,98 +1,139 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Button, Col, Input, Layout, List, Row} from 'antd';
 import {clrs} from "../../constants/colors";
 import HeaderPlatform from "../../components/HeaderPlatform/HeaderPlatform";
 import Message from "./Message";
 import icon from "../../images/user.svg"
+import Block from '../../components/UI/Block/Block';
+import FlexBlock from '../../components/UI/FlexBlock/FlexBlock';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigate, useParams} from 'react-router-dom';
+import {API_BASE_URL} from '../../constants/api';
+import {ACCESS_TOKEN} from '../../constants/auth';
+import Card from '../../components/LoadingComponents/Card';
+import {getChatById} from '../../store/slices/chat/chatSlice';
+import BigText from '../../components/UI/BigText/BigText';
+import {lan} from '../../constants/lan';
+import ChatService from '../../services/ChatService';
 
 const {Content} = Layout;
 
 const ChatPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    const {user} = useSelector(state => state.user);
+
+    const {chat, isLoading} = useSelector(state => state.chat);
+
+    const [messageList, setMessageList] = useState([]);
+    //-------------
+    const [stompClient, setStompClient] = useState(null);
+
+    useEffect(() => {
+        if (chat !== null) {
+            console.log(chat);
+            setMessageList(chat.messageList);
+        }
+    }, [chat])
+
+    useEffect(() => {
+        let socket = new SockJS(API_BASE_URL + '/ws');
+        setStompClient(Stomp.over(socket));
+        dispatch(getChatById({id}))
+    }, [navigate]);
+
+    useEffect(() => {
+        if (stompClient !== null) {
+            stompClient.connect({}, function (frame) {
+                stompClient.subscribe(
+                    `/chat/${id}`,
+                    function (greeting) {
+                        let message = JSON.parse(greeting.body);
+                        console.log(message);
+                        setMessageList([...messageList, message]);
+                    },
+                    {
+                        'chatId': `${id}`,
+                        'Authorization': `${localStorage.getItem(ACCESS_TOKEN)}`
+                    },
+                );
+            });
+        }
+    }, [stompClient])
+
+    //------------------------
+
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([
-        {
-            id: 2,
-            text: 'Hello Batyrbek!',
-            name: 'Dias',
-            time: '9:50 AM'
-        },
-        {
-            id: 1,
-            text: 'Hello Dias!',
-            name: 'Batyrbek',
-            time: '9:51 AM'
-        },
-        {
-            id: 2,
-            text: 'Doner?',
-            name: 'Dias',
-            time: '9:52 AM'
-        },
-        {
-            id: 1,
-            text: 'Of course, 667!',
-            name: 'Batyrbek',
-            time: '9:53 AM'
-        },
-        {
-            id: 1,
-            text: 'Of course, 667!',
-            name: 'Batyrbek',
-            time: '9:53 AM'
-        },
-        {
-            id: 1,
-            text: 'Of course, 667!',
-            name: 'Batyrbek',
-            time: '9:53 AM'
-        },
-        {
-            id: 1,
-            text: 'Of course, 667!',
-            name: 'Batyrbek',
-            time: '9:53 AM'
-        },
-    ]);
-    const messageListRef = useRef(null);
 
     const handleMessageChange = (e) => {
         setMessage(e.target.value);
     };
 
-    const handleSendMessage = () => {
-        if (message.trim() !== '') {
-            setMessages([...messages, message]);
-            setMessage('');
-        }
-    };
+    function handleSendMessage(e) {
+        e.preventDefault();
+        ChatService.sendMessageToChat(id, message)
+            .then((r) => {
+                setMessageList([...messageList, r.data]);
+            })
+        setMessage('');
+    }
 
-    useEffect(() => {
-        messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
-    }, [messages]);
+    if (isLoading) {
+        return (
+            <div style={{backgroundColor: clrs.whiter, width: '100%', minHeight: '100vh'}}>
+                <HeaderPlatform/>
+                <Block style={{marginTop: "50px", alignItems: "flex-start"}}>
+                    <Card type={"horizontal"}/>
+                    <Card type={"horizontal-small"}/>
+                </Block>
+            </div>
+        )
+    }
+
+    if (chat === null) {
+        return (
+            <div style={{backgroundColor: clrs.whiter, width: "100%", minHeight: "100vh"}}>
+                <HeaderPlatform/>
+                <Block style={{marginTop: "50px"}}>
+                    <BigText>{lan.chatNotFound}</BigText>
+                </Block>
+            </div>
+        );
+    }
+
 
     return (
         <div style={{backgroundColor: clrs.whiter, width: '100%', minHeight: '100vh'}}>
             <HeaderPlatform/>
-            <Layout>
-                <Content style={{padding: '16px', marginTop: '65px'}}>
-                    <div
-                        ref={messageListRef}
-                        style={{
-                            maxHeight: 'calc(100vh - 130px)',
-                            overflowY: 'auto',
-                            marginBottom: '16px',
-                        }}
-                    >
+            <Block style={{marginTop: "50px", alignItems: "flex-start"}}>
+                <FlexBlock style={{
+                    height: "calc(100vh - 50px - 50px - 100px - 50px)",
+                    flexDirection: "column",
+                    backgroundColor: clrs.white,
+                    borderRadius: "15px",
+                    padding: "20px",
+                    width: "calc(100% - 40px)",
+                }}>
+                    <FlexBlock style={{overflowY: "auto", height: "100%", backgroundColor: clrs.whiter,}}>
                         <List
-                            dataSource={messages}
+                            style={{
+                                width: "calc(100% - 30px)",
+                                padding: "25px 15px",
+                                height: "100%"
+                        }}
+                            dataSource={messageList}
                             renderItem={(item) => (
-                                item.id === 1 ?
+                                item.user.id === user.id ?
                                     <Row justify='end' align="bottom">
                                         <Col>
                                             <Message
-                                                sender={item.name}
-                                                content={item.text}
-                                                timestamp={item.time}
+                                                sender={item.user.username}
+                                                content={item.message}
+                                                timestamp={item.createdAt}
                                             />
                                         </Col>
                                         <Col>
@@ -106,36 +147,37 @@ const ChatPage = () => {
                                         </Col>
                                         <Col>
                                             <Message
-                                                sender={item.name}
-                                                content={item.text}
-                                                timestamp={item.time}
+                                                sender={item.user.username}
+                                                content={item.message}
+                                                timestamp={item.createdAt}
                                             />
                                         </Col>
                                     </Row>
                             )}
                         />
-                    </div>
-                    <div
-                        style={{
-                            position: 'fixed',
-                            bottom: '16px',
-                            left: '16px',
-                            right: '16px',
+                    </FlexBlock>
+
+                    <FlexBlock>
+                        <form style={{
                             display: 'flex',
-                        }}
-                    >
-                        <Input
-                            value={message}
-                            onChange={handleMessageChange}
-                            placeholder="Type your message"
-                            style={{flex: 1, marginRight: '16px'}}
-                        />
-                        <Button type="primary" onClick={handleSendMessage}>
-                            Send
-                        </Button>
-                    </div>
-                </Content>
-            </Layout>
+                            gap: "20px",
+                            width: '100%'
+                        }} onSubmit={handleSendMessage}>
+                            <Input
+                                value={message}
+                                onChange={handleMessageChange}
+                                placeholder="Type your message"
+                                style={{flex: 1, marginRight: '16px'}}
+                            />
+                            <Button type="primary">
+                                Send
+                            </Button>
+                        </form>
+
+                    </FlexBlock>
+                </FlexBlock>
+
+            </Block>
         </div>
     );
 };
