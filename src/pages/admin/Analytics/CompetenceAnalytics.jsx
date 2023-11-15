fa5ccb7163e195b20a349163d04783c957116934
Batyrbek <bakytkerey.b@gmail.com>
2023-11-15 15:50:29 +0600

    download stats

--- src/pages/admin/Analytics/CompetenceAnalytics.jsx
+++ src/pages/admin/Analytics/CompetenceAnalytics.jsx
@@ -10,6 +10,11 @@ import Card from '../../../components/LoadingComponents/Card';
 import Button from '../../../components/UI/Button/Button';
 import {LocalName} from '../../../utils/LocalName';
 import HorizontalDivider from '../../../components/UI/Divider/HorizontalDivider';
+import * as XLSX from 'xlsx';
+import UserService from "../../../services/UserService";
+import AdminUserService from "../../../services/AdminUserService";
+import CompetenceService from "../../../services/CompetenceService";
+import {message} from "antd";
 
 const CompetenceAnalytics = () => {
 
@@ -22,6 +27,60 @@ const CompetenceAnalytics = () => {
     }, [analytics]);
 
     const [selectedComponent, setSelectedComponent] = useState(null);
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const generateExcelFile = async () => {
+        await AdminUserService.getAllUsers(1, 1000)
+            .then(async (r) => {
+                messageApi.open({
+                    type: 'loading',
+                    content: 'Загрузка...',
+                    duration: 0,
+                });
+                console.log(r.data.length)
+                let usersList = [];
+                for (const user of r.data) {
+                    await CompetenceService.getCompetenceBankByUserId(user.id)
+                        .then((r) => {
+                            usersList.push(
+                                {
+                                    "ФИО": user.fullName,
+                                    "Имя пользователя": user.username,
+                                    "Университет": user.university?.name,
+                                    "Дидактический":Number((r.data.componentBankList[0]?.averagePoint / r.data.componentBankList[0]?.maxPoint * 100).toFixed(2)),
+                                    "Проектировочный":Number((r.data.componentBankList[1]?.averagePoint / r.data.componentBankList[1]?.maxPoint * 100).toFixed(2)),
+                                    "Коммуникативный":Number((r.data.componentBankList[2]?.averagePoint / r.data.componentBankList[2]?.maxPoint * 100).toFixed(2)),
+                                    "Рефлексивный":Number((r.data.componentBankList[3]?.averagePoint / r.data.componentBankList[3]?.maxPoint * 100).toFixed(2)),
+                                    "Мониторинговый":Number((r.data.componentBankList[4]?.averagePoint / r.data.componentBankList[4]?.maxPoint * 100).toFixed(2)),
+                                    "Личностно-мотивационный":Number((r.data.componentBankList[5]?.averagePoint / r.data.componentBankList[5]?.maxPoint * 100).toFixed(2)),
+                                }
+                            )
+                        })
+                        .catch(() => {
+
+                        })
+                }
+                const ws = XLSX.utils.json_to_sheet(usersList);
+
+                // Create a workbook
+                const wb = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+                // Save the file
+                XLSX.writeFile(wb, 'stats.xlsx');
+
+                messageApi.destroy();
+                messageApi.open({
+                    type: 'success',
+                    content: 'Успешно',
+                    duration: 5,
+                });
+            })
+            .catch(() => {
+
+            })
+    };
 
     const [config, setConfig] = useState(
         {
@@ -145,6 +204,8 @@ const CompetenceAnalytics = () => {
 
     return (
         <FlexBlock style={{flexDirection: "column", alignItems: "flex-start"}}>
+            {contextHolder}
+            <Button onClick={generateExcelFile} type={2}>Скачать статистику</Button>
             <Text default>{lan.components} :</Text>
             <FlexBlock style={{flexWrap: "wrap", justifyContent: "flex-start"}}>
                 {analytics?.components.map((component, i) => {
